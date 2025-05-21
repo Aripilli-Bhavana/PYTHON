f474f3533eedf672870f0ce206b970ba82d0bccc
Aripilli Bhavana <aripilli.bhavana8@gmail.com>
2025-05-21 23:54:51 +0530

    Initial commit with basic Python programs

--- BASICS/tuple.py
+++ BASICS/tuple.py
@@ -67,3 +67,13 @@ print(yellow)
 print(red)
 
 
+#using asterick *
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+
